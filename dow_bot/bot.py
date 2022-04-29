1f847758d8c83cb49bafbfb655f84ec09d184947
dow1226 <dow1226@gmail.com>
2022-04-29 11:45:38 +0800

    fix dice add multiple player session rebuild structure

--- dow_bot/bot.py
+++ dow_bot/bot.py
@@ -38,4 +38,4 @@ for filename in os.listdir('./cmds'):
         bot.load_extension(f'cmds.{filename[:-3]}')
 
 if __name__ == '__main__':
-    bot.run('OTY1NjM2MTY4MzU0MDA0OTky.Yl2Eww.u01PaEYLPTHkyZ4KnDiy5sCKdG0')
+    bot.run(TOKEN)
